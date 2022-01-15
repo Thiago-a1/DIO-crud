323a7e450434510e42079686616d122af435f0df
thiago araujo <thiago.araujo.r11@gmail.com>
2022-01-14 22:41:24 -0300

    add basic-authentication.middleware

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,37 +1,17 @@
 import { NextFunction, Request, Response, Router } from 'express';
-import ForbiddenError from '../models/errors/forbidden.error.model';
-import userRepository from '../repositories/user.repository';
-import JWT from 'jsonwebtoken'; 
 import { StatusCodes } from 'http-status-codes';
+import JWT from 'jsonwebtoken';
+import basicAuthenticationMiddleware from '../middlewares/basic-authentication.middleware';
+import ForbiddenError from '../models/errors/forbidden.error.model';
 
 const authorizationRoute = Router();
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
+authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const authorizationHeader = req.headers['authorization'];
+    const user = req.user;
 
-    if(!authorizationHeader) {
-      throw new ForbiddenError('Credenciais não informadas');
-    }
-
-    const [authenticationType, token] = authorizationHeader.split(' ');
-
-    if(authenticationType !== 'Basic' || !token) {
-      throw new ForbiddenError('Tipo de authenticação invalido');
-    }
-
-    const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
-
-    const [username, password] = tokenContent.split(':');
-
-    if(!username || !password) {
-      throw new ForbiddenError('credenciais  não preenchidas');
-    }
-
-    const user = await userRepository.findByUsernameAndPassword(username, password);
-
-    if(!user) {
-      throw new ForbiddenError('Usuario ou senha invalidos!');
+    if (!user) {
+      throw new ForbiddenError('Usuario não informado!');
     }
 
     const jwtPayLoad = { username: user.username };
@@ -39,7 +19,7 @@ authorizationRoute.post('/token', async (req: Request, res: Response, next: Next
     const secretKey = 'my_secret_key';
 
     const jwt = JWT.sign(jwtPayLoad, secretKey, jwtOptions);
-    
+
     res.status(StatusCodes.OK).json({ token: jwt });
   } catch (error) {
     next(error);
